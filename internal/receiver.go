92f0e66c8b74f2b47cec117f8b82745f761f17f0
Tristan Penman <tristan@tristanpenman.com>
2022-03-02 17:01:04 +1100

    WIP receiver

--- internal/receiver.go
+++ internal/receiver.go
@@ -1,19 +1,55 @@
 package internal
 
-import "encoding/json"
+import (
+	"encoding/json"
+)
 
 const receiverNamespace = "urn:x-cast:com.google.cast.receiver"
 
-func (clientConnection *ClientConnection) handleGetAppAvailability() {
+type receiverMessage struct {
+	Type string `json:"type"`
+}
 
+type getAppAvailabilityRequest struct {
+	AppId []string `json:"appId"`
+}
+
+type getAppAvailabilityResponse struct {
+}
+
+func (clientConnection *ClientConnection) handleGetAppAvailability(data string) {
+	var request getAppAvailabilityRequest
+	err := json.Unmarshal([]byte(data), &request)
+	if err != nil {
+		clientConnection.log.Error("failed to connect data", "err", err)
+		return
+	}
+}
+
+type getStatusRequest struct {
+}
+
+type getStatusResponse struct {
 }
 
 func (clientConnection *ClientConnection) handleGetStatus() {
 
 }
 
+type launchRequest struct {
+}
+
+type launchResponse struct {
+}
+
 func (clientConnection *ClientConnection) handleLaunch() {
 
+}
+
+type stopRequest struct {
+}
+
+type stopResponse struct {
 }
 
 func (clientConnection *ClientConnection) handleStop() {
@@ -21,21 +57,16 @@ func (clientConnection *ClientConnection) handleStop() {
 }
 
 func (clientConnection *ClientConnection) handleReceiverMessage(data string) {
-	var parsed map[string]*string
+	var parsed receiverMessage
 	err := json.Unmarshal([]byte(data), &parsed)
 	if err != nil {
 		clientConnection.log.Error("failed to parse receiver message", "err", err)
 		return
 	}
 
-	dType := parsed["type"]
-	if dType == nil {
-		clientConnection.log.Error("receiver message type missing")
-	}
-
-	switch *dType {
+	switch parsed.Type {
 	case "GET_APP_AVAILABILITY":
-		clientConnection.handleGetAppAvailability()
+		clientConnection.handleGetAppAvailability(data)
 		break
 	case "GET_STATUS":
 		clientConnection.handleGetStatus()
@@ -47,7 +78,7 @@ func (clientConnection *ClientConnection) handleReceiverMessage(data string) {
 		clientConnection.handleStop()
 		break
 	default:
-		clientConnection.log.Error("unknown receiver message type", "type", dType)
+		clientConnection.log.Error("unknown receiver message type", "type", parsed.Type)
 		break
 	}
 }
